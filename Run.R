9131604201ada18569472338543fc63be31e65e7
weaversd <sweaver4@nd.edu>
2021-09-06 12:54:50 -0400

    added table png output'

--- Run.R
+++ Run.R
@@ -5,13 +5,25 @@ source("predict_game2.R")
 source("Simulate_season.R")
 source("simulate_many_seasons.R")
 source("Final Summary.R")
+
 magic_plot
 out
 print_final()
 
+library("htmltools")
+library("webshot")
+
 current_table_output <- subset(current_table, select = -(`Pos Trend`))
 write.table(current_table_output, paste0("current_table_output/Matchweek_", matchdays, "_table.csv"),
             sep = ",", row.names = F)
+
+
+path <- html_print(out, background = "white", viewer = NULL)
+url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
+webshot(url,
+        file = paste0("current_table_output/WCQ_table.png"),
+        selector = ".formattable_widget",
+        delay = 0.2)
 
 
 source("Weekly_Analysis.R")
